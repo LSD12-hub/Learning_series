00c97027bbefff0640e7e66d62beaefb068500e8
ljonas021 <ljonas021@student.wethinkcode.co.za>
2022-07-29 22:39:02 +0200

    learing js

--- gallery-start/gallery-start/main.js
+++ gallery-start/gallery-start/main.js
@@ -11,10 +11,23 @@ const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"]
 for ( let i = 0; i < images.length; i++ ) {
     // const element = array[i];
     const newImage = document.createElement('img');
-    newImage.setAttribute( 'src', "images/"+images[i] );
+    newImage.setAttribute( 'src', "images/" + images[i] );
+    newImage.addEventListener( "click" , function(){
+        displayedImage.setAttribute( "src", this.src )
+    });
     thumbBar.appendChild( newImage );
-    
 }
 
-
+let dimmed = false;
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener( "click", function() {
+
+    if (dimmed) {
+        overlay.style.backgroundColor = "rgba(0, 0, 0, 0)";
+        dimmed = false;
+    } else {
+        overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        dimmed = true;
+    }
+});
+
